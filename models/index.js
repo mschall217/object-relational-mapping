3043761eb2c65bacc569afaf588e1a7eee0bc5dc
Morgan Schall <morgan.allison.schall@gmail.com>
2021-05-11 07:17:36 -0400

    didn't save the models I updated

--- models/index.js
+++ models/index.js
@@ -1,31 +1,26 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
-})
-// Categories have many Products
+	foreignKey: "category_id",
+});
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
-})
-// Products belongToMany Tags (through ProductTag)
+	foreignKey: "category_id",
+});
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_tag'
-})
-// Tags belongToMany Products (through ProductTag)
+	through: ProductTag,
+	foreignKey: "product_tag",
+});
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id',
-})
+	through: ProductTag,
+	foreignKey: "tag_id",
+});
 
 module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
+	Product,
+	Category,
+	Tag,
+	ProductTag,
 };
