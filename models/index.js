2986359f3a7fba867e294b26b6fdcfed37840600
Morgan Schall <morgan.allison.schall@gmail.com>
2021-05-11 07:35:40 -0400

    fixed bug in models that wasn't displaying tags

--- models/index.js
+++ models/index.js
@@ -4,14 +4,14 @@ const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
 Product.belongsTo(Category, {
-	foreignKey: "category_id",
+	foreignKey: "category_id"
 });
 Category.hasMany(Product, {
-	foreignKey: "category_id",
+	foreignKey: "category_id"
 });
 Product.belongsToMany(Tag, {
 	through: ProductTag,
-	foreignKey: "product_tag",
+	foreignKey: "product_id"
 });
 Tag.belongsToMany(Product, {
 	through: ProductTag,
